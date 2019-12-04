23e97ba84997f146edf7d059aa339520c2900b25
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2019-12-04 19:21:13 +0100

    Geomath

---
+++ App/APM/Local.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Essentials;
+
+namespace APM
+{
+    class Local
+    {
+        private const string fileName = "Accesspoints.json";
+        private const string filePath = Path.Combine(FileSystem.AppDataDirectory, fileName);
+
+        public static void saveToFile(string json)
+        {
+            File.WriteAllText(filePath, json);
+        }
+    }
+}
