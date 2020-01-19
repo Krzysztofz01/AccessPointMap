367842f1a0284aad2b9073f6c4e322ad9f0ce92d
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2020-01-19 19:55:00 +0100

    Frontend style, api calls

---
+++ Server/1.3.b/api/request/read.php
@@ -0,0 +1,53 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+
+    //Database connection
+    include_once "../config/database.php";
+
+    //Init the model
+    include_once "../models/AccessPoint.php";
+
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $accessPoint = new AccessPoint($db);
+
+    $stmt = $accessPoint->read();
+    $num = $stmt->rowCount();
+
+    if($num>0) {
+        $accessPointArray = array();
+        $accessPointArray["records"] = array();
+
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            extract($row);
+
+            $accessPointItem = array(
+                "id" => $id,
+                "bssid" => $bssid,
+                "ssid" => $ssid,
+                "freq" => $freq,
+                "signalLevel" => $signalLevel,
+                "latitude" => $latitude,
+                "longitude" => $longitude,
+                "security" => $security,
+                "vendor" => $vendor
+            );
+
+            array_push($accessPointArray["records"], $accessPointItem);
+        }
+
+        //Data encoded to JSON, OK
+        http_response_code(200);
+
+        //Show data in JSON format
+        echo json_encode($accessPointArray);
+    } else {
+        //Not data found
+        http_response_code(404);
+
+        //Showing the message that no items were found
+        echo json_encode(array("message" => "No items found!"));
+    }
+?>
---
+++ Server/1.3.b/app/fileHandler.php
@@ -0,0 +1,49 @@
+<?php
+
+if(isset($_FILES['jsonFile'])) {
+    $fileError = array();
+    $fileName = $_FILES['jsonFile']['name'];
+    $fileTmp = $_FILES['jsonFile']['tmp_name'];
+    $fileSize = $_FILES['jsonFile']['size'];
+    $fileExt = strtolower(end(explode('.', $fileName)));
+
+    if($fileExt != "json") {
+        $fileError[] = "Extension error!";
+    }
+
+    if($fileSize > 8388608) {
+        $fileError[] = "Size error!";
+    }
+
+    if(empty($fileError)) {
+        move_uploaded_file($fileTmp, "json/".$fileName);
+
+        $jsonString = file_get_contents("json/".$fileName);
+
+        $jsonArray = json_decode($jsonString, true);
+
+        for($i = 0; $i < sizeof($jsonArray); $i++) {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, "../api/request/add.php");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-type: application/json"));
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($jsonArray[i]));
+
+            $response = curl_exec($ch);
+
+            if(curl_getinfo($ch, CURLINFO_HTTP_CODE) != 201) {
+                $fileError[] = "API call error on ". i ."!";
+            }
+
+            curl_close($ch);
+        }
+
+        if(empty($fileError)) {
+            unlink("json/".$fileName);
+        }
+    }
+
+    unset($_FILES['jsonFile']);
+}
+?>
