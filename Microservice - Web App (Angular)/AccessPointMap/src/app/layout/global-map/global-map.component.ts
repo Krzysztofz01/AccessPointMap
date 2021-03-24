9208d4dc92858192e1179196030b327109256159
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2021-03-24 19:19:45 +0100

    Global map filter, cleanup and scan device code

--- Microservice - Web App (Angular)/AccessPointMap/src/app/layout/global-map/global-map.component.ts
+++ Microservice - Web App (Angular)/AccessPointMap/src/app/layout/global-map/global-map.component.ts
@@ -14,9 +14,9 @@ import { Accesspoint } from 'src/app/models/accesspoint.model';
 import { CacheManagerService } from 'src/app/services/cache-manager.service';
 import { SelectedAccesspointService } from 'src/app/services/selected-accesspoint.service';
 import { AccesspointDataService } from 'src/app/services/accesspoint-data.service';
-import { LocalStorageOptions } from 'src/app/models/local-storage-options.model';
 import { environment } from 'src/environments/environment';
 import { ErrorHandlingService } from 'src/app/services/error-handling.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'global-map',
@@ -25,47 +25,127 @@ import { ErrorHandlingService } from 'src/app/services/error-handling.service';
 })
 export class GlobalMapComponent implements OnInit {
   private map: Map;
+  private vectorLayerName: String = "marker_layer";
   public popupAccesspoints: Array<object>;
   public popupIndicator: boolean;
+  public securitySelect: FormGroup;
+  public securityOptions: Array<String> = ['All', 'WPA2', 'WPA', 'WEP', 'WPS', 'None'];
 
   constructor(private cacheService: CacheManagerService, private selectedAccesspoint: SelectedAccesspointService, private accesspointDataService: AccesspointDataService, private errorHandlingService: ErrorHandlingService) { }
 
   ngOnInit(): void {
-    this.initializeMapData();
+    //Initialize the FormGroup which contains the security select element
+    this.securitySelect = new FormGroup({
+      security: new FormControl("All")
+    });
 
+    //Subscribe to the select value changes
+    this.securitySelect.get("security").valueChanges
+      .subscribe((value) => {
+
+        //Initialize map data and tell the method that the map IS initialized
+        this.initializeMapData(true);
+        //Reset the popup variables
+        this.popupIndicator = false;
+        this.popupAccesspoints = undefined;
+    });
+
+    //Initialize map data and tell the method that the map IS NOT initialized yet
+    this.initializeMapData(false);
+    //Reset the popup variables
     this.popupIndicator = false;
     this.popupAccesspoints = undefined;
   }
 
-  private initializeMapData(): void {
+  //Get accesspoints from cache or from the API and than apply filtres and cache the data
+  private initializeMapData(isInitialized: boolean): void {
     const accesspoints: Array<Accesspoint> = this.cacheService.load(environment.CACHE_ACCESSPOINTS);
-    const featuresArray = new Array<Feature>();
+    //const featuresArray = new Array<Feature>();
+
     if(accesspoints != null) {
-      accesspoints.forEach(element => featuresArray.push(this.prepareSingleMarker(element)));
-      this.initializeMap(featuresArray);
+      //Data is cached in local storage      
+      //Initialize the map with generated features or swap vector layer
+      if(isInitialized) {
+        this.swapVectorLayer(this.applySecurityFilter(accesspoints));
+      } else {
+        this.initializeMap(this.applySecurityFilter(accesspoints));
+      }
+      
     } else {
-      this.accesspointDataService.getAllAccessPoints().toPromise()
-        .then(x => {
-          x.forEach(element => {
-            featuresArray.push(this.prepareSingleMarker(element));
-          });
-          this.initializeMap(featuresArray);
+      //Data is not cached or out of date
+      //Get accesspoiint from API
+      this.accesspointDataService.getAllAccessPoints()
+        .subscribe((accesspoints) => {
+          //Initialize the map with generated features or swap vector layer
+            if(isInitialized) {
+              this.swapVectorLayer(this.applySecurityFilter(accesspoints));
+            } else {
+              this.initializeMap(this.applySecurityFilter(accesspoints));
+            }
+
+          //Delete the old cache record and cache the response
           this.cacheService.delete(environment.CACHE_ACCESSPOINTS);
-          const cacheOptions: LocalStorageOptions = { key: environment.CACHE_ACCESSPOINTS, data: x, expirationMinutes: 60 };
-          this.cacheService.save(cacheOptions);
-        })
-        .catch(error => {
+          this.cacheService.save({ key: environment.CACHE_ACCESSPOINTS, data: accesspoints, expirationMinutes: 60 });
+        },
+        (error) => {
           console.log(error);
           this.errorHandlingService.setException(`${error.name} ${error.statusText}`);
         });
     }
   }
 
+  private swapVectorLayer(featuresArray: Array<Feature>): void {
+    //Find and delete the old marker layer
+    this.map.getLayers().forEach(layer => {
+      if(layer && layer.get('name') === this.vectorLayerName) {
+        this.map.removeLayer(layer);
+      }
+    });
+
+    //Create the vector layer object and set the name using external method
+    const vectorLayer: VectorLayer = new VectorLayer({
+      source: new VectorSource({
+        features: featuresArray
+      })
+    });
+
+    vectorLayer.set('name', this.vectorLayerName);
+
+    //Add the layer with new markers to map
+    this.map.addLayer(vectorLayer);
+  }
+
+  //Return an array on accesspoint features based on the selected security type
+  private applySecurityFilter(accesspoints: Array<Accesspoint>): Array<Feature> {
+    const features: Array<Feature> = [];
+    const selectedSecurity = this.securitySelect.get('security').value;
+
+    accesspoints.forEach(x => {
+      //Parse security JSON data from single accesspoint
+      const data: Array<String> = JSON.parse(x.securityData);
+      
+      //Security types comparision logic
+      switch(selectedSecurity) {
+        case 'All': features.push(this.prepareSingleMarker(x)); break;
+        case 'WPA2': if(data.includes('WPA2')) features.push(this.prepareSingleMarker(x)); break;
+        case 'WPA': if(!data.includes('WPA2') && data.includes('WPA')) features.push(this.prepareSingleMarker(x)); break;
+        case 'WEP': if(!data.includes('WPA2') && !data.includes('WPA') && data.includes('WEP')) features.push(this.prepareSingleMarker(x)); break;
+        case 'WPS': if(!data.includes('WPA2') && !data.includes('WPA') && data.includes('WPS')) features.push(this.prepareSingleMarker(x)); break;
+        case 'None': if(!data.includes('WPA2') && !data.includes('WPA') && !data.includes('WEP') && !data.includes('WPS')) features.push(this.prepareSingleMarker(x)); break;
+        default: break;
+      }
+    });
+
+    return features;
+  }
+
+  //Map initialization method that takes the generated features
   private initializeMap(featuresArray: Array<Feature>): void {
-    //Map popup
+    //Vanilla JS to init the popup HTML elements
     const container = document.getElementById('popup');
     const closer = document.getElementById('popup-closer');
 
+    //Overlay for the popup
     const overlay: Overlay = new Overlay({
       element: container,
       autoPan: true,
@@ -74,13 +154,23 @@ export class GlobalMapComponent implements OnInit {
       }
     });
 
+    //Popup close event
     closer.onclick = () => {
       overlay.setPosition(undefined);
       closer.blur();
       return false;
     };
 
-    //Map element
+    //Create the vector layer object and set the name using external method
+    const vectorLayer: VectorLayer = new VectorLayer({
+      source: new VectorSource({
+        features: featuresArray
+      })
+    });
+
+    vectorLayer.set('name', this.vectorLayerName);
+
+    //The base map object initialization
     this.map = new Map({
       controls: [],
       target: 'global-map',
@@ -88,11 +178,7 @@ export class GlobalMapComponent implements OnInit {
         new TileLayer({
           source: new OSM()
         }),
-        new VectorLayer({
-          source: new VectorSource({
-            features: featuresArray
-          })
-        })
+        vectorLayer
       ],
       overlays: [
         overlay
@@ -103,19 +189,22 @@ export class GlobalMapComponent implements OnInit {
       })
     });
 
-    //Map click event
+    //Marker click event for popup and behavior subject
     this.map.on('click', (e) => {
       this.popupIndicator = false;
       this.popupAccesspoints = undefined;
       
+      //Get all accesspoints on clicked pixel
       const selectedAccessPoints: Array<Accesspoint> = new Array<Accesspoint>();
       this.map.forEachFeatureAtPixel(e.pixel, (feature, layer) => {
         selectedAccessPoints.push(feature.values_.attributes.accesspoint);
       });
 
+      //Check if there are any selected accesspoints
       if(selectedAccessPoints.length > 0) {
         this.popupAccesspoints = [];
 
+        //Envoke the behavior subject if there is only one accesspoint, show popup on many accesspoints or show error if there is more than 10 accesspoints
         if(selectedAccessPoints.length == 1) {
           this.popupAccesspoints = undefined;
           this.selectedAccesspoint.changeAccesspoint(selectedAccessPoints[0]);
@@ -133,10 +222,12 @@ export class GlobalMapComponent implements OnInit {
     });
   }
 
+  //Wrapper method for behavior subject service
   public setAccesspoint(accesspoint: Accesspoint ) : void {
     this.selectedAccesspoint.changeAccesspoint(accesspoint);
   }
 
+  //Prepare a signle feature object based on given accesspoint object
   private prepareSingleMarker(accesspoint: Accesspoint) : Feature {
     const feat : Feature = new Feature({
       geometry: new Point(olProj.fromLonLat([accesspoint.highLongitude, accesspoint.highLatitude])),
@@ -153,6 +244,7 @@ export class GlobalMapComponent implements OnInit {
     return feat;
   }
 
+  //Get map pin color based on security or visiblity of given accesspoint
   private getPinColor(accesspoint: Accesspoint) : string {
     const green = 'http://cdn.mapmarker.io/api/v1/pin?size=50&hoffset=1&background=%2368BC00';
     const yellow = 'http://cdn.mapmarker.io/api/v1/pin?size=50&hoffset=1&background=%23FB9E00';
--- Scanning device (ESP8266)/apm.ino
+++ Scanning device (ESP8266)/apm.ino
@@ -1,31 +1,98 @@
+//Gps data parser library
+#include <TinyGPS++.h>
+
+//ESP8266 Wifi Driver libraries
+#include <BearSSLHelpers.h>
+#include <CertStoreBearSSL.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266WiFiAP.h>
+#include <ESP8266WiFiGeneric.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266WiFiScan.h>
+#include <ESP8266WiFiSTA.h>
+#include <ESP8266WiFiType.h>
+#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
+#include <WiFiClientSecureAxTLS.h>
+#include <WiFiClientSecureBearSSL.h>
+#include <WiFiServer.h>
+#include <WiFiServerSecure.h>
+#include <WiFiServerSecureAxTLS.h>
+#include <WiFiServerSecureBearSSL.h>
+#include <WiFiUdp.h>
+
+//Multiple virtual serial ports library
+#include <SoftwareSerial.h>
+
+TinyGPSPlus gps;
+SoftwareSerial gpsUart(4, 5);
+
+//Geolocation variables
+float latitude = NULL;
+float longitude = NULL;
+
+//LED indicator pins
+const int WAIT_FOR_GPS_AVAILABLE = 0;
+const int SCAN_CYCLE = 2;
 
 void setup() {
+  //UART communication setup
   Serial.begin(115200);
-  Serial.println("AccessPointMap - Setup...");
+  gpsUart.begin(9600);
+
+  //Wifi scanner setip
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
-  delay(2000);
-  Serial.println("AccessPointMap - Setup done.");
 
+  //Indicator LED setup
+  pinMode(WAIT_FOR_GPS_AVAILABLE, OUTPUT);
+  digitalWrite(WAIT_FOR_GPS_AVAILABLE, HIGH);
+  
+  pinMode(SCAN_CYCLE, OUTPUT);
+  digitalWrite(SCAN_CYCLE, LOW);
+  
+  
+  delay(2000);
+  Serial.println("Setup finished!");
 }
 
 void loop() {
-  Serial.println("Scanning...");
-  int accesspoints = WiFi.scanNetworks();
-  if(accesspoints == 0) {
-    Serial.println("No networks found.");    
-  } else {
-    Serial.println("##########################");
-    for(int i = 0; i < accesspoints; i++) {
-      Serial.print(WiFi.BSSIDstr(i));
-      Serial.print(" ");
-      Serial.print(WiFi.SSID(i));
-      Serial.print(" ");
-      Serial.print(WiFi.RSSI(i));
-      Serial.print(" ");
-      Serial.print(WiFi.encryptionType(i));
-      Serial.println(" ");
+  //Check if data on GPS serial port is available
+  while(gpsUart.available() > 0) {
+    //Encode GPS data if correct
+    if(gps.encode(gpsUart.read())) {
+      if(gps.location.isValid()) {
+        latitude = gps.location.lat();
+        longitude = gps.location.lng();
+      }
     }
   }
+
+  //Proceed if GPS connection is established
+  if(latitude != NULL && longitude != NULL) {
+    digitalWrite(WAIT_FOR_GPS_AVAILABLE, LOW);
+    indicateScanCycle();
+
+    //Get number of available accesspoints
+    int accesspoints = WiFi.scanNetworks();
+
+    if(accesspoints > 0) {
+      for(int i=0; i < accesspoints; i++) {
+        Serial.println("{\"bssid\": \"" + WiFi.BSSIDstr(i) + "\", \"ssid\": \"" + WiFi.SSID(i) + "\", \"signalLevel\": \"" + WiFi.RSSI(i) + "\", \"securityDataRaw\": \"" + getEncryptionType(WiFi.encryptionType(i)) + "\",  \"latitude\": \"" + String(latitude, 6) + "\", \"longitude\": \"" + String(longitude, 6) + "\"}");
+      }
+    }
+    Serial.println("");
+  }
+}
+
+void indicateScanCycle() {
+  digitalWrite(SCAN_CYCLE, HIGH);
+  delay(5);
+  digitalWrite(SCAN_CYCLE, LOW);
+}
+
+String getEncryptionType(int type) {
+  if(type == 2 || type == 4) return "WPA";
+  if(type == 5) return "WEP";
+  return "None";
 }
