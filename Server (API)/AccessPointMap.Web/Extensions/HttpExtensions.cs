f45336ecb95bf2002e632598ec9a3899e5b4bcfb
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2021-08-20 20:45:31 +0200

    Custom middlewares and .net5

---
+++ Server (API)/AccessPointMap.Web/Extensions/HttpExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+
+namespace AccessPointMap.Web.Extensions
+{
+    public static class HttpExtensions
+    {
+        public static Uri GetUri(this HttpRequest request)
+        {
+            var uriBuilder = new UriBuilder
+            {
+                Scheme = request.Scheme,
+                Host = request.Host.Host,
+                Port = request.Host.Port.GetValueOrDefault(80),
+                Path = request.Path.ToString(),
+                Query = request.QueryString.ToString()
+            };
+
+            return uriBuilder.Uri;
+        }
+    }
+}
---
+++ Server (API)/AccessPointMap.Web/Middleware/TelemetryMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using AccessPointMap.Service.Maintenance;
+using AccessPointMap.Service.Maintenance.Models;
+using AccessPointMap.Web.Extensions;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace AccessPointMap.Web.Middleware
+{
+    public class TelemetryMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ITelemetryService _telemetryService;
+
+        public TelemetryMiddleware(
+            RequestDelegate next,
+            ITelemetryService telemetryService)
+        {
+            _next = next ??
+                throw new ArgumentNullException(nameof(next));
+
+            _telemetryService = telemetryService ??
+                throw new ArgumentNullException(nameof(telemetryService));
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string requestUrl = context.Request.GetUri().ToString();
+
+            await _next(context);
+
+            await _telemetryService.LogEvent(Report.Factory.Create(requestUrl, nameof(TelemetryMiddleware)));
+        }
+    }
+}
