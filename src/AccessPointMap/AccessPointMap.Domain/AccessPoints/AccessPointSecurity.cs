bf915e0d3d2951e96c8b16691228cf3db9f22733
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2021-11-30 17:41:26 +0100

    Geolocation and security related value objects

---
+++ src/AccessPointMap/AccessPointMap.Domain/AccessPoints/AccessPointSecurity.cs
@@ -0,0 +1,70 @@
+﻿using AccessPointMap.Domain.Core.Extensions;
+using AccessPointMap.Domain.Core.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+
+namespace AccessPointMap.Domain.AccessPoints
+{
+    public class AccessPointSecurity : ValueObject<AccessPointSecurity>
+    {
+        private readonly IReadOnlyCollection<EncryptionType> _encryptionTypes = new List<EncryptionType>
+        {
+            new EncryptionType { Name = "WPA3", IsSecure = true, Priority = 10 },
+            new EncryptionType { Name = "WPA2", IsSecure = true, Priority = 9 },
+            new EncryptionType { Name = "WPA", IsSecure = true, Priority = 8 },
+            new EncryptionType { Name = "WPS", IsSecure = false, Priority = 7 },
+            new EncryptionType { Name = "WEP", IsSecure = false, Priority = 6 }
+        };
+
+        public string RawSecurityPayload { get; private set; }
+        public string SerializedSecurityPayload { get; private set; }
+        
+        public bool IsSecure { get; private set; }
+
+        private AccessPointSecurity() { }
+        private AccessPointSecurity(string rawSecurityPayload)
+        {
+            if (rawSecurityPayload.IsEmpty())
+            {
+                RawSecurityPayload = string.Empty;
+                SerializedSecurityPayload = "[]";
+                IsSecure = false;
+            }
+            else
+            {
+                RawSecurityPayload = rawSecurityPayload.Trim().ToUpper();
+
+                var usedEncryptionTypes = new List<EncryptionType>();
+
+                foreach (var type in _encryptionTypes)
+                {
+                    if (RawSecurityPayload.Contains(type.Name))
+                        usedEncryptionTypes.Add(type);
+                }
+
+                if (usedEncryptionTypes.Count > 0)
+                {
+                    SerializedSecurityPayload = JsonSerializer.Serialize(usedEncryptionTypes.Select(t => t.Name));
+
+                    IsSecure = usedEncryptionTypes.OrderByDescending(t => t.Priority).First().IsSecure;
+                }
+                else
+                {
+                    SerializedSecurityPayload = "[]";
+                    
+                    IsSecure = false;
+                }
+            }
+        }
+
+        public static AccessPointSecurity FromString(string rawSecurityPayload) => new AccessPointSecurity(rawSecurityPayload);
+    }
+
+    internal class EncryptionType
+    {
+        public string Name { get; init; }
+        public bool IsSecure { get; init; }
+        public int Priority { get; init; }
+    }
+}
---
+++ src/AccessPointMap/AccessPointMap.Domain/AccessPoints/AccessPointSsid.cs
@@ -0,0 +1,32 @@
+﻿using AccessPointMap.Domain.Core.Exceptions;
+using AccessPointMap.Domain.Core.Models;
+
+namespace AccessPointMap.Domain.AccessPoints
+{
+    public class AccessPointSsid : ValueObject<AccessPointSsid>
+    {
+        private const string _hiddenNetworkName = "Hidden network";
+
+        public string Value { get; private set; }
+
+        private AccessPointSsid() { }
+        private AccessPointSsid(string value)
+        {
+            if (value is null)
+                throw new ValueObjectValidationExceptions("Invalid access point ssid value.");
+
+            if (value == string.Empty)
+            {
+                Value = _hiddenNetworkName;
+                return;
+            }
+
+            Value = value;
+        }
+
+        public static implicit operator string(AccessPointSsid ssid) => ssid.Value;
+
+        public static AccessPointSsid FromString(string ssid) => new AccessPointSsid(ssid);
+        public static AccessPointSsid Hidden => new AccessPointSsid(string.Empty);
+    }
+}
