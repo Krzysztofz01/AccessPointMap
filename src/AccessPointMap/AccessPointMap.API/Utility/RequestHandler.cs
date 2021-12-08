eec964f3b1c0bb5f44d7d4bc31e02338be8230bb
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2021-12-08 23:33:31 +0100

    Request utility and middleware

---
+++ src/AccessPointMap/AccessPointMap.API/Utility/RequestHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using AccessPointMap.Application.Abstraction;
+using AccessPointMap.Domain.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace AccessPointMap.API.Utility
+{
+    public static class RequestHandler
+    {
+        public static async Task<IActionResult> Command<TAggregateRoot>(IApplicationCommand<TAggregateRoot> command, Func<IApplicationCommand<TAggregateRoot>, Task> serviceHandler) where TAggregateRoot : AggregateRoot
+        {
+            await serviceHandler(command);
+
+            return new OkResult();
+        }
+
+        public static async Task<IActionResult> MapQuery<TResponse, TResponseDto>(Task<TResponse> query, IMapper mapper)
+        {
+            var result = await query;
+
+            if (result is null) return new NotFoundResult();
+
+            var mappedResult = mapper.Map<TResponseDto>(result);
+
+            return new OkObjectResult(mappedResult);
+        }
+    }
+}
