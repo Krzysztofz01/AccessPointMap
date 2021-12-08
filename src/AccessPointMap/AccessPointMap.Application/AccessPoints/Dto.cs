380b909f88fbd033b9c1a3d03e5248ad785db40d
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2021-12-08 13:34:48 +0100

    Access point contracts, dtos and mapping

---
+++ src/AccessPointMap/AccessPointMap.Application/AccessPoints/Dto.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AccessPointMap.Application.AccessPoints
+{
+    public static class Dto
+    {
+        public class AccessPointSimple
+        {
+            public Guid Id { get; set; }
+            public string Bssid { get; set; }
+            public string Ssid { get; set; }
+            public double Frequency { get; set; }
+            public string DeviceType { get; set; }
+            public double HighSignalLatitude { get; set; }
+            public double HighSignalLongitude { get; set; }
+            public double SignalArea { get; set; }
+            public string SerializedSecurityPayload { get; set; }
+            public bool IsSecure { get; set; }
+        }
+
+        public class AccessPointDetailsUnauthorized
+        {
+            public Guid Id { get; set; }
+            public string Bssid { get; set; }
+            public string Manufacturer { get; set; }
+            public string Ssid { get; set; }
+            public double Frequency { get; set; }
+            public string DeviceType { get; set; }
+            public Guid ContributorId { get; set; }
+            public DateTime CreationTimestamp { get; set; }
+            public DateTime VersionTimestamp { get; set; }
+            public int LowSignalLevel { get; set; }
+            public double LowSignalLatitude { get; set; }
+            public double LowSignalLongitude { get; set; }
+            public int HighSignalLevel { get; set; }
+            public double HighSignalLatitude { get; set; }
+            public double HighSignalLongitude { get; set; }
+            public double SignalRadius { get; set; }
+            public double SignalArea { get; set; }
+            public string RawSecurityPayload { get; set; }
+            public string SerializedSecurityPayload { get; set; }
+            public bool IsSecure { get; set; }
+            public IEnumerable<AccessPointStampDetails> Stamps { get; set; }
+        }
+
+        public class AccessPointDetailsAuthorized
+        {
+            public Guid Id { get; set; }
+            public string Bssid { get; set; }
+            public string Manufacturer { get; set; }
+            public string Ssid { get; set; }
+            public double Frequency { get; set; }
+            public string DeviceType { get; set; }
+            public Guid ContributorId { get; set; }
+            public DateTime CreationTimestamp { get; set; }
+            public DateTime VersionTimestamp { get; set; }
+            public int LowSignalLevel { get; set; }
+            public double LowSignalLatitude { get; set; }
+            public double LowSignalLongitude { get; set; }
+            public int HighSignalLevel { get; set; }
+            public double HighSignalLatitude { get; set; }
+            public double HighSignalLongitude { get; set; }
+            public double SignalRadius { get; set; }
+            public double SignalArea { get; set; }
+            public string RawSecurityPayload { get; set; }
+            public string SerializedSecurityPayload { get; set; }
+            public bool IsSecure { get; set; }
+            public string Note { get; set; }
+            public string DisplayStatus { get; set; }
+            public IEnumerable<AccessPointStampDetails> Stamps { get; set; }
+        }
+
+        public class AccessPointStampDetails
+        {
+            public Guid Id { get; set; }
+            public string Ssid { get; set; }
+            public double Frequency { get; set; }
+            public string DeviceType { get; set; }
+            public Guid ContributorId { get; set; }
+            public DateTime CreationTimestamp { get; set; }
+            public int LowSignalLevel { get; set; }
+            public double LowSignalLatitude { get; set; }
+            public double LowSignalLongitude { get; set; }
+            public int HighSignalLevel { get; set; }
+            public double HighSignalLatitude { get; set; }
+            public double HighSignalLongitude { get; set; }
+            public double SignalRadius { get; set; }
+            public double SignalArea { get; set; }
+            public string RawSecurityPayload { get; set; }
+            public string SerializedSecurityPayload { get; set; }
+            public bool IsSecure { get; set; }
+            public bool Status { get; set; }
+        }
+    }
+}
