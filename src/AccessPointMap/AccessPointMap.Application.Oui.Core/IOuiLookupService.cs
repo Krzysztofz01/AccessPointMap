45e34d0b2276b761cbcc3a49a79da84bd8692de3
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2021-12-12 17:19:46 +0100

    OUI lookup abstraction

---
+++ src/AccessPointMap/AccessPointMap.Application.Oui.Core/IOuiLookupService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace AccessPointMap.Application.Oui.Core
+{
+    public interface IOuiLookupService
+    {
+        Task<string> GetManufacturerName(string macAddress);
+    }
+}
