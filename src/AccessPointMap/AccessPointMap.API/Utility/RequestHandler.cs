179ce9edae3fa28c2aeab50c2d9554fec951c7b0
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2021-12-15 13:43:50 +0100

    Request handler refactor

--- src/AccessPointMap/AccessPointMap.API/Utility/RequestHandler.cs
+++ src/AccessPointMap/AccessPointMap.API/Utility/RequestHandler.cs
@@ -17,6 +17,52 @@ namespace AccessPointMap.API.Utility
             return new OkResult();
         }
 
+        public static async Task<IActionResult> ExecuteService<TRequest>(TRequest request, Func<TRequest, Task> serviceHandler)
+        {
+            await serviceHandler(request);
+
+            return new OkResult();
+        }
+
+        public static async Task<TResponse> CachedQuery<TResponse>(Task<TResponse> query, string key, IMemoryCache memoryCache) where TResponse : class
+        {
+            if (memoryCache.TryGetValue(key, out TResponse cachedResult)) return cachedResult;
+
+            var queriedResult = await query;
+
+            if (queriedResult is null) return null;
+
+            var cacheOptions = new MemoryCacheEntryOptions()
+                .SetAbsoluteExpiration(DateTime.Now.AddMinutes(30))
+                .SetSlidingExpiration(TimeSpan.FromMinutes(2))
+                .SetPriority(CacheItemPriority.High);
+
+            memoryCache.Set(key, queriedResult, cacheOptions);
+
+            return queriedResult;
+        }
+
+        public static async Task<TResponse> DirectQuery<TResponse>(Task<TResponse> query) where TResponse : class
+        {
+            var result = await query;
+
+            if (result is null) return null;
+
+            return result;
+        }
+
+        public static async Task<IActionResult> Map<TResponse, TDto>(Task<TResponse> query, IMapper mapper)
+        {
+            var result = await query;
+
+            if (result is null) return new NotFoundResult();
+
+            var mappedResult = mapper.Map<TDto>(result);
+
+            return new OkObjectResult(mappedResult);
+        }
+
+        [Obsolete]
         public static async Task<IActionResult> MapQuery<TResponse, TResponseDto>(Task<TResponse> query, IMapper mapper)
         {
             var result = await query;
@@ -28,36 +74,7 @@ namespace AccessPointMap.API.Utility
             return new OkObjectResult(mappedResult);
         }
 
-        public static async Task<IActionResult> MapCachedQuery<TResponse, TResponseDto>(Task<TResponse> query, IMapper mapper, string endpoint, IMemoryCache memoryCache)
-        {
-            memoryCache.TryGetValue(endpoint, out string response);
-
-            if (!string.IsNullOrEmpty(response)) return new OkObjectResult(response);
-
-            var result = await query;
-
-            var cacheOptions = new MemoryCacheEntryOptions
-            {
-                AbsoluteExpiration = DateTime.Now.AddMinutes(5),
-                Priority = CacheItemPriority.Normal,
-                SlidingExpiration = TimeSpan.FromMinutes(2),
-                Size = 4096
-            };
-
-            if (result is null)
-            {
-                memoryCache.Set(endpoint, result, cacheOptions);
-
-                return new NotFoundResult();
-            }
-
-            var mappedResult = mapper.Map<TResponseDto>(result);
-
-            memoryCache.Set(endpoint, mappedResult, cacheOptions);
-
-            return new OkObjectResult(mappedResult);
-        }
-
+        [Obsolete]
         public static async Task<IActionResult> IntegrationServiceCommand<TRequest>(TRequest request, Func<TRequest, Task> serviceMethod)
         {
             await serviceMethod(request);
