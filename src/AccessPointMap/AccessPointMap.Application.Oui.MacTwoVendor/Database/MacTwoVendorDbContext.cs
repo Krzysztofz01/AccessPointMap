b01e1531c027e3c364b5162d077f4aef5ff5da49
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2021-12-16 13:01:38 +0100

    MacTwoVendor fix

--- src/AccessPointMap/AccessPointMap.Application.Oui.MacTwoVendor/Database/MacTwoVendorDbContext.cs
+++ src/AccessPointMap/AccessPointMap.Application.Oui.MacTwoVendor/Database/MacTwoVendorDbContext.cs
@@ -1,25 +1,12 @@
 ﻿using AccessPointMap.Application.Oui.MacToVendor.Database.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.IO;
 
 namespace AccessPointMap.Application.Oui.MacToVendor.Database
 {
     public class MacTwoVendorDbContext : DbContext
     {
-        private readonly string _databasePath;
-        private readonly string _databaseFileName = "oui-database.sqlite";
-
         public DbSet<Vendor> Vendors { get; set; }
 
-        public MacTwoVendorDbContext()
-        {
-            var assemblyPath = AppDomain.CurrentDomain.BaseDirectory;
-
-            _databasePath = Path.Combine(assemblyPath, _databaseFileName);
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
-            options.UseSqlite($"Data Source={_databasePath}");
+        public MacTwoVendorDbContext(DbContextOptions<MacTwoVendorDbContext> options) : base(options) { }
     }
 }
