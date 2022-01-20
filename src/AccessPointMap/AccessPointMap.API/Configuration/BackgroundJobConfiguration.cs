8dfcce780e325fea84421211b324f38e0545636e
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2022-01-20 14:46:15 +0100

    Quartz implementation

--- src/AccessPointMap/AccessPointMap.API/Configuration/BackgroundJobConfiguration.cs
+++ src/AccessPointMap/AccessPointMap.API/Configuration/BackgroundJobConfiguration.cs
@@ -6,14 +6,46 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Quartz;
 using System;
 
 namespace AccessPointMap.API.Configuration
 {
     public static class BackgroundJobConfiguration
     {
+        private const string _schedulerId = "AccessPointMapQuartz";
+
         public static IServiceCollection AddBackgroundJobs(this IServiceCollection services)
         {
+            services.Configure<QuartzOptions>(options =>
+            {
+                options.Scheduling.IgnoreDuplicates = true;
+                options.Scheduling.OverWriteExistingData = true;
+            });
+
+            services.AddQuartz(options =>
+            {
+                options.SchedulerId = _schedulerId; 
+
+                options.UseMicrosoftDependencyInjectionJobFactory();
+                options.UseSimpleTypeLoader();
+                options.UseInMemoryStore();
+                options.UseDefaultThreadPool(tpool =>
+                {
+                    tpool.MaxConcurrency = 10;
+                });
+
+                options.ScheduleJob<AccessPointManufacturerJob>(trigger => trigger
+                    .WithIdentity(AccessPointManufacturerJob.JobName)
+                    .WithCronSchedule(AccessPointManufacturerJob.CronExpression)
+                );
+            });
+
+            services.AddQuartzServer(options =>
+            {
+                options.WaitForJobsToComplete = true;
+            });
+
             services.AddHangfire(options =>
             {
                 options
@@ -37,8 +69,8 @@ namespace AccessPointMap.API.Configuration
                 app.UseHangfireDashboard();
             }
 
-            jobs.AddOrUpdate("AccessPointUpdateManufacturer", () => service
-                .GetService<IAccessPointBackgroundJobs>().SetAccessPointManufacturer(), Cron.Daily);
+            /*jobs.AddOrUpdate("AccessPointUpdateManufacturer", () => service
+                .GetService<IAccessPointBackgroundJobs>().SetAccessPointManufacturer(), Cron.Daily);*/
 
             return app;
         }
---
+++ src/AccessPointMap/AccessPointMap.Application/AccessPoints/AccessPointManufacturerJob.cs
@@ -0,0 +1,69 @@
+﻿using AccessPointMap.Application.Oui.Core;
+using AccessPointMap.Domain.AccessPoints;
+using AccessPointMap.Infrastructure.Core.Abstraction;
+using Microsoft.Extensions.Logging;
+using Quartz;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccessPointMap.Application.AccessPoints
+{
+    [DisallowConcurrentExecution]
+    public class AccessPointManufacturerJob : IJob
+    {
+        public const string CronExpression = "0 0 0/6 1/1 * ? *";
+        public const string JobName = "AccessPointManufacturerUpdate";
+
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IDataAccess _dataAccess;
+        private readonly IOuiLookupService _ouiLookupService;
+        private readonly ILogger<AccessPointBackgroundJobs> _logger;
+
+        public AccessPointManufacturerJob(IUnitOfWork unitOfWork, IDataAccess dataAccess, IOuiLookupService ouiLookupService, ILogger<AccessPointBackgroundJobs> logger)
+        {
+            _unitOfWork = unitOfWork ??
+                throw new ArgumentNullException(nameof(unitOfWork));
+
+            _dataAccess = dataAccess ??
+                throw new ArgumentNullException(nameof(dataAccess));
+
+            _ouiLookupService = ouiLookupService ??
+                throw new ArgumentNullException(nameof(ouiLookupService));
+
+            _logger = logger ??
+                throw new ArgumentNullException(nameof(logger));
+        }
+
+        [Obsolete("This method has inconsistent usage of data access abstraction.")]
+        public async Task Execute(IJobExecutionContext context)
+        {
+            try
+            {
+                _logger.LogInformation($"{JobName} job started.");
+
+                var accessPoints = _dataAccess.AccessPointsTracked
+                    .Where(a => a.Manufacturer.Value == string.Empty);
+
+                foreach (var accessPoint in accessPoints)
+                {
+                    var manufacturerName = await _ouiLookupService.GetManufacturerName(accessPoint.Bssid.Value);
+
+                    accessPoint.Apply(new Events.V1.AccessPointManufacturerChanged
+                    {
+                        Id = accessPoint.Id,
+                        Manufacturer = manufacturerName
+                    });
+                }
+
+                await _unitOfWork.Commit();
+
+                _logger.LogInformation($"{JobName} job finished.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{JobName} job failed.", ex);
+            }
+        }
+    }
+}
