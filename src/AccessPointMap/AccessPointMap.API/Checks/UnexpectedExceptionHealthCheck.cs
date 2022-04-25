8736197d4d0089a2de677861cb3165cbed78298e
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2022-04-25 19:13:17 +0200

    Health check implementation

---
+++ src/AccessPointMap/AccessPointMap.API/Checks/UnexpectedExceptionHealthCheck.cs
@@ -0,0 +1,25 @@
+﻿using AccessPointMap.API.Services;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AccessPointMap.API.Checks
+{
+    internal class UnexpectedExceptionHealthCheck : IHealthCheck
+    {
+        private readonly IHealthStatusService _healthStatusService;
+
+        public UnexpectedExceptionHealthCheck(IHealthStatusService healthStatusService) =>
+            _healthStatusService = healthStatusService ?? throw new ArgumentNullException(nameof(healthStatusService));
+
+
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            if (_healthStatusService.IsHealthy())
+                return Task.FromResult(HealthCheckResult.Healthy());
+
+            return Task.FromResult(HealthCheckResult.Degraded());
+        }
+    }
+}
---
+++ src/AccessPointMap/AccessPointMap.API/Configuration/HealthCheckConfiguration.cs
@@ -0,0 +1,53 @@
+﻿using AccessPointMap.API.Checks;
+using AccessPointMap.API.Services;
+using AccessPointMap.Application.Settings;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace AccessPointMap.API.Configuration
+{
+    public static class HealthCheckConfiguration
+    {
+        public const string _healthCheckPath = "/health";
+
+        public static IServiceCollection AddServiceHealthChecks(this IServiceCollection services, IConfiguration configuration)
+        {
+            var healthCheckSettingsSection = configuration
+                .GetSection(nameof(HealthCheckSettings));
+
+            services.Configure<HealthCheckSettings>(healthCheckSettingsSection);
+            var healthCheckSettings = healthCheckSettingsSection.Get<HealthCheckSettings>();
+
+            services.AddSingleton<IHealthStatusService, HealthStatusService>();
+
+            if (healthCheckSettings.Enabled)
+            {
+                services.AddHealthChecks()
+                    .AddCheck<DatabaseConnectionHealthCheck>(nameof(DatabaseConnectionHealthCheck))
+                    .AddCheck<UnexpectedExceptionHealthCheck>(nameof(UnexpectedExceptionHealthCheck));
+            }
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseServiceHealthChecks(this IApplicationBuilder app, IServiceProvider service)
+        {
+            var healthCheckSettings = service.GetService<IOptions<HealthCheckSettings>>().Value ??
+                throw new InvalidOperationException("Healthcheck settings are not available.");
+
+            if (healthCheckSettings.Enabled)
+            {
+                app.UseHealthChecks(_healthCheckPath, new HealthCheckOptions
+                {
+                    AllowCachingResponses = false
+                });
+            }
+
+            return app;
+        }
+    }
+}
