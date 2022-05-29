fd4216c603f5524a9c925892074f71d0c5203350
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-05-29 20:00:54 +0200

    Adnnotation entity and value object implementation

---
+++ src/AccessPointMap/AccessPointMap.Domain/AccessPoints/AccessPointAdnnotations/AccessPointAdnnotationTimestamp.cs
@@ -0,0 +1,15 @@
+﻿using AccessPointMap.Domain.Shared;
+using System;
+
+namespace AccessPointMap.Domain.AccessPoints.AccessPointAdnnotations
+{
+    public class AccessPointAdnnotationTimestamp : Timestamp 
+    {
+        protected AccessPointAdnnotationTimestamp() { }
+        protected AccessPointAdnnotationTimestamp(DateTime value) : base(value) { }
+
+        public static implicit operator DateTime(AccessPointAdnnotationTimestamp adnnotationTimestamp) => adnnotationTimestamp.Value;
+
+        public static AccessPointAdnnotationTimestamp Now => new AccessPointAdnnotationTimestamp(DateTime.Now);
+    }
+}
