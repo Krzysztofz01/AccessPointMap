496c7c4da997c3738266774621aa90ef884f0cff
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-06-21 18:54:10 +0200

    Pcap core service contracts implementation

---
+++ src/AccessPointMap/AccessPointMap.Application.Pcap.Core/IPcapParsingService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AccessPointMap.Application.Pcap.Core
+{
+    public interface IPcapParsingService
+    {
+        public Task<IDictionary<string, IList<Packet>>> MapPacketsToMacAddresses(IFormFile pcapFile);
+    }
+}
---
+++ src/AccessPointMap/AccessPointMap.Application.Pcap.Core/Packet.cs
@@ -0,0 +1,9 @@
+﻿namespace AccessPointMap.Application.Pcap.Core
+{
+    public class Packet
+    {
+        public string SourceAddress { get; init; }
+        public string DestinationAddress { get; init; }
+        public string Data { get; init; }
+    }
+}
