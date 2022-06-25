897f6b93fcafc88608c13415a5ccd4f51e3c1cef
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-06-25 15:46:31 +0200

    Implementation of native pcap parser

---
+++ src/AccessPointMap/AccessPointMap.Application.Pcap.ApmPcapNative/ApmPcapNativePcapParsingService.cs
@@ -0,0 +1,16 @@
+﻿using AccessPointMap.Application.Pcap.Core;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AccessPointMap.Application.Pcap.ApmPcapNative
+{
+    public class ApmPcapNativePcapParsingService : IPcapParsingService
+    {
+        public Task<IDictionary<string, IList<Packet>>> MapPacketsToMacAddresses(IFormFile pcapFile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/AccessPointMap/AccessPointMap.Application.Pcap.ApmPcapNative/Extensions/ByteArrayExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AccessPointMap.Application.Pcap.ApmPcapNative.Extensions
+{
+    internal static class ByteArrayExtensions
+    {
+        public static BinaryReader ToBinaryReader(this byte[] byteArray)
+        {
+            using var memoryStream = new MemoryStream(byteArray);
+            return new BinaryReader(memoryStream);
+        }
+
+        public static ushort ToUInt16(this byte[] byteArray, int offset = 0)
+        {
+            return BitConverter.ToUInt16(byteArray, offset);
+        }
+
+        public static string ToBase64HardwareAddress(this byte[] byteArray, int offset = 0)
+        {
+            var bufferSlice = byteArray.Skip(offset).Take(6).ToArray();
+
+            return Convert.ToBase64String(bufferSlice);
+        }
+    }
+}
