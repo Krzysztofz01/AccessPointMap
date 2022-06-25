4793562c1d23ffd637cd64fc0d23f3f0b26e1c4d
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-06-25 17:16:38 +0200

    MySQL infrastructure migration for SubType support

---
+++ src/AccessPointMap/AccessPointMap.Infrastructure.MySql/Migrations/20220625151442_AccessPointPacketSubTypeImplementation.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccessPointMap.Infrastructure.MySql.Migrations
+{
+    public partial class AccessPointPacketSubTypeImplementation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Subtype_Value",
+                schema: "apm",
+                table: "AccessPointPacket",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Subtype_Value",
+                schema: "apm",
+                table: "AccessPointPacket");
+        }
+    }
+}
