9df134d611fb4293461bf8c21298a9b95937c2ce
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-06-25 17:11:28 +0200

    AccessPointPacket subtype implementation

---
+++ src/AccessPointMap/AccessPointMap.Domain/AccessPoints/AccessPointPackets/AccessPointPacketSubtype.cs
@@ -0,0 +1,23 @@
+﻿using AccessPointMap.Domain.Core.Exceptions;
+using AccessPointMap.Domain.Core.Models;
+
+namespace AccessPointMap.Domain.AccessPoints.AccessPointPackets
+{
+    public class AccessPointPacketSubtype : ValueObject<AccessPointPacketSubtype>
+    {
+        public string Value { get; private set; }
+
+        private AccessPointPacketSubtype() { }
+        private AccessPointPacketSubtype(ushort value)
+        {
+            if (!Constants.Ieee80211FrameSubTypeToName.ContainsKey(value))
+                throw new ValueObjectValidationException("Invalid IEEE802.11 frame subtype identifier.");
+
+            Value = Constants.Ieee80211FrameSubTypeToName[value];
+        }
+
+        public static implicit operator string(AccessPointPacketSubtype subtype) => subtype.Value;
+
+        public static AccessPointPacketSubtype FromUInt16(ushort subtype) => new AccessPointPacketSubtype(subtype);
+    }
+}
--- src/AccessPointMap/AccessPointMap.Domain/AccessPoints/Constants.cs
+++ src/AccessPointMap/AccessPointMap.Domain/AccessPoints/Constants.cs
@@ -23,6 +23,26 @@ namespace AccessPointMap.Domain.AccessPoints
             new EncryptionType { Name = "WPS", IsSecure = false, Priority = 7 },
             new EncryptionType { Name = "WEP", IsSecure = false, Priority = 6 }
         };
+
+        public static readonly IReadOnlyDictionary<ushort, string> Ieee80211FrameSubTypeToName = new Dictionary<ushort, string>
+        {
+            { 0x0019, "Block Ack" },
+            { 0x001d, "Acknowledgement" },
+            { 0x000d, "Action" },
+            { 0x0008, "Beacon Frame" },
+            { 0x001c, "Clear-to-send" },
+            { 0x0020, "Data" },
+            { 0x0024, "Null function" },
+            { 0x0005, "Probe response" },
+            { 0x0028, "QoS Data" },
+            { 0x001b, "Request-to-send" }
+        };
+
+        public static IEnumerable<ushort> Ieee80211FrameSubTypesWithoutHardwareAddresses = new List<ushort>
+        {
+            0x001d,
+            0x001c
+        };
     }
 
     public class EncryptionType
