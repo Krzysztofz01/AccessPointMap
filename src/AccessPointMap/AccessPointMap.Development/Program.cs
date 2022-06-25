0873d8b22ac0ca966863c58d2c5df124b97da322
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-06-25 16:33:42 +0200

    Development purpose startup project for pcap tests

---
+++ src/AccessPointMap/AccessPointMap.Development/Program.cs
@@ -0,0 +1,16 @@
+﻿using AccessPointMap.Application.Pcap.ApmPcapNative;
+using System.IO;
+
+namespace AccessPointMap.Development
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            const string pcapFilePath = @"D:\Pobrane\apm-pcap.cap";
+
+            using var pcapParser = new PcapParser(File.OpenRead(pcapFilePath));
+            pcapParser.ParsePackets();
+        }
+    }
+}
