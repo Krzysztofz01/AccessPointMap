f8ef5dc89ff7226c7611ef4a0e3c4a6a2c27b3d4
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-06-25 15:46:47 +0200

    Ieee80211 constant values

---
+++ src/AccessPointMap/AccessPointMap.Application.Pcap.ApmPcapNative/Constants.cs
@@ -0,0 +1,16 @@
+﻿namespace AccessPointMap.Application.Pcap.ApmPcapNative
+{
+    internal class Constants
+    {
+        public const int PcapFileHeaderLength = 24;
+
+        public const int FrameHeaderStartToLengthOffset = 8;
+
+        public const int FrameHeaderLengthToEndOffset = 6;
+
+        public const int FrameHeaderLength = FrameHeaderStartToLengthOffset + FrameHeaderLengthToEndOffset + sizeof(ushort);
+
+        public const int Ieee80211BlockAckDestinationOffset = 4;
+        public const int Ieee80211BlockAckSourceOffset = 10;
+    }
+}
---
+++ src/AccessPointMap/AccessPointMap.Application.Pcap.ApmPcapNative/Ieee80211SubTypes.cs
@@ -0,0 +1,15 @@
+﻿namespace AccessPointMap.Application.Pcap.ApmPcapNative
+{
+    internal enum Ieee80211SubTypes: ushort
+    {
+        BlockAck = 0x0019,
+        Acknowledgement = 0x001d,
+        Action = 0x000d,
+        Beacon = 0x0008,
+        ClearToSend = 0x001c,
+        Data = 0x0020,
+        NullFunction = 0x0024,
+        Probe = 0x0005,
+        Qos = 0x0028
+    }
+}
