fcb2b334deed61db2297e3f34f1872069b5d68d4
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-06-28 20:42:16 +0200

    Braeking changes WiGLE support

---
+++ src/AccessPointMap/AccessPointMap.Application.Integration.Wigle/Commands.cs
@@ -0,0 +1,13 @@
+﻿using AccessPointMap.Application.Integration.Core;
+using Microsoft.AspNetCore.Http;
+
+namespace AccessPointMap.Application.Integration.Wigle
+{
+    public static class Commands
+    {
+        public class CreateAccessPointsFromCsvFile : IIntegrationCommand
+        {
+            public IFormFile ScanCsvFile { get; set; }
+        }
+    }
+}
