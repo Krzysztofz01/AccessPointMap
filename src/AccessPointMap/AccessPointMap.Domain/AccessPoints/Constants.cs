9482cb75145f24f0fdf0663f42a88e9cd387c00a
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-06-30 16:09:20 +0200

    Security values transformed into lookup dict

--- src/AccessPointMap/AccessPointMap.Domain/AccessPoints/Constants.cs
+++ src/AccessPointMap/AccessPointMap.Domain/AccessPoints/Constants.cs
@@ -15,66 +15,52 @@ namespace AccessPointMap.Domain.AccessPoints
             { new string[] { "car" }, "Car" }
         };
 
-        public static readonly IReadOnlyCollection<EncryptionType> EncryptionTypes = new List<EncryptionType>
-        {
-            new EncryptionType { Name = "WPA3", IsSecure = true, Priority = 10 },
-            new EncryptionType { Name = "WPA2", IsSecure = true, Priority = 9 },
-            new EncryptionType { Name = "WPA", IsSecure = true, Priority = 8 },
-            new EncryptionType { Name = "WPS", IsSecure = false, Priority = 7 },
-            new EncryptionType { Name = "WEP", IsSecure = false, Priority = 6 }
-        };
 
-        public static readonly IReadOnlyCollection<SecurityStandard> SecurityStandards = new List<SecurityStandard>
+        public static readonly IReadOnlyDictionary<string, SecurityProtocol> SecurityProtocols = new Dictionary<string, SecurityProtocol>
         {
-            new SecurityStandard { Name = "WEP", FullName = "Wired Equivalent Privacy", IsSecure = false, Priority = 10 },
-            new SecurityStandard { Name = "WEP2", FullName = "Wired Equivalent Privacy 2", IsSecure = false, Priority = 20 },
-            new SecurityStandard { Name = "WEP+", FullName = "Wired Equivalent Privacy Plus", IsSecure = false, Priority = 30 },
-            new SecurityStandard { Name = "Dynamic WEP", FullName = "Dynamic Wired Equivalent Privacy", IsSecure = false, Priority = 40 },
-            new SecurityStandard { Name = "WPS", FullName = "Wi-Fi Simple Config", IsSecure = false, Priority = 50 },
-            new SecurityStandard { Name = "WPA", FullName = "Wi-Fi Protected Access", IsSecure = false, Priority = 60 },
-            new SecurityStandard { Name = "WPA2", FullName = "Wi-Fi Protected Access II", IsSecure = true, Priority = 70 },
-            new SecurityStandard { Name = "WPA3", FullName = "Wi-Fi Protected Access 3", IsSecure = true, Priority = 80 }
-        };
+            // Standards
+            { "WEP",  new SecurityProtocol { Name = "WEP", FullName = "Wired Equivalent Privacy", IsSecure = false, Type = SecurityProtocolType.Framework, Priority = 10 } },
+            { "WEP2", new SecurityProtocol { Name = "WEP2", FullName = "Wired Equivalent Privacy 2", IsSecure = false, Type = SecurityProtocolType.Framework, Priority = 20 } },
+            { "WEP+", new SecurityProtocol { Name = "WEP+", FullName = "Wired Equivalent Privacy Plus", IsSecure = false, Type = SecurityProtocolType.Framework, Priority = 30 } },
+            { "Dynamic WEP", new SecurityProtocol { Name = "Dynamic WEP", FullName = "Dynamic Wired Equivalent Privacy", IsSecure = false, Type = SecurityProtocolType.Framework, Priority = 40 } },
+            { "WPS", new SecurityProtocol { Name = "WPS", FullName = "Wi-Fi Simple Config", IsSecure = false, Type = SecurityProtocolType.Framework, Priority = 50 } },
+            { "WPA", new SecurityProtocol { Name = "WPA", FullName = "Wi-Fi Protected Access", IsSecure = false, Type = SecurityProtocolType.Framework, Priority = 60 } },
+            { "WPA2", new SecurityProtocol { Name = "WPA2", FullName = "Wi-Fi Protected Access II", IsSecure = true, Type = SecurityProtocolType.Framework, Priority = 70 } },
+            { "WPA3", new SecurityProtocol { Name = "WPA3", FullName = "Wi-Fi Protected Access 3", IsSecure = true, Type = SecurityProtocolType.Framework, Priority = 80 } },
 
-        public static readonly IReadOnlyCollection<SecurityProtocols> SecurityProtocols = new List<SecurityProtocols>
-        {
-            new SecurityProtocols { Name = "CRC-32", FullName = "Cyclic redundancy check", IsSecure = false, Usage = "Integraity" },
-            new SecurityProtocols { Name = "RC4", FullName = "Rivest Cipher 4", IsSecure = false, Usage = "Encryption" },
-            new SecurityProtocols { Name = "TKIP", FullName = "Temporal Key Integrity Protocol", IsSecure = false, Usage = "Integrity" },
-            new SecurityProtocols { Name = "PSK", FullName = "Pre-shared key", IsSecure = false, Usage = "Key exchange" },
-            new SecurityProtocols { Name = "CCMP", FullName = "Counter Mode CBC-MAC Protocol", IsSecure = true, Usage = "Encryption" },
-            new SecurityProtocols { Name = "AES", FullName = "Advanced Encryption Standard", IsSecure = true, Usage = "Encryption" },
-            new SecurityProtocols { Name = "SHA", FullName = "Secure Hash Algorithm", IsSecure = true, Usage = "Integraity" },
-            new SecurityProtocols { Name = "GCM", FullName = "Galois/Counter Mode", IsSecure = true, Usage = "Encryption Mode" },
-            new SecurityProtocols { Name = "CCM", FullName = "Counter with CBC-MAC", IsSecure = true, Usage = "Encryption Mode" },
-            new SecurityProtocols { Name = "SAE", FullName = "Simultaneous Authentication of Equals", IsSecure = true, Usage = "Key exchange" },
-            new SecurityProtocols { Name = "PBKDF", FullName = "Password-Based Key Derivation Function", IsSecure = true, Usage = "Key derivation" },
-            new SecurityProtocols { Name = "RADIUS", FullName = "Remote Authentication Dial-In User Service", IsSecure = true, Usage = "Authentication" },
-            new SecurityProtocols { Name = "EAP", FullName = "Counter with CBC-MAC", IsSecure = true, Usage = "Authentication" },
-            new SecurityProtocols { Name = "CCM", FullName = "Counter with CBC-MAC", IsSecure = true, Usage = "Encryption Mode" },
+            // Protocols
+            { "CRC-32", new SecurityProtocol { Name = "CRC-32", FullName = "Cyclic redundancy check", IsSecure = false, Type = SecurityProtocolType.Integrity } },
+            { "RC4", new SecurityProtocol { Name = "RC4", FullName = "Rivest Cipher 4", IsSecure = false, Type = SecurityProtocolType.Encryption } },
+            { "TKIP", new SecurityProtocol { Name = "TKIP", FullName = "Temporal Key Integrity Protocol", IsSecure = false, Type = SecurityProtocolType.Integrity } },
+            { "PSK", new SecurityProtocol { Name = "PSK", FullName = "Pre-shared key", IsSecure = false, Type = SecurityProtocolType.KeyExchange } },
+            { "CCMP", new SecurityProtocol { Name = "CCMP", FullName = "Counter Mode CBC-MAC Protocol", IsSecure = true, Type = SecurityProtocolType.Encryption } },
+            { "AES", new SecurityProtocol { Name = "AES", FullName = "Advanced Encryption Standard", IsSecure = true, Type = SecurityProtocolType.Encryption } },
+            { "SHA", new SecurityProtocol { Name = "SHA", FullName = "Secure Hash Algorithm", IsSecure = true, Type = SecurityProtocolType.Integrity } },
+            { "GCM", new SecurityProtocol { Name = "GCM", FullName = "Galois/Counter Mode", IsSecure = true, Type = SecurityProtocolType.EncryptionMode } },
+            { "CCM", new SecurityProtocol { Name = "CCM", FullName = "Counter with CBC-MAC", IsSecure = true, Type = SecurityProtocolType.EncryptionMode } },
+            { "SAE", new SecurityProtocol { Name = "SAE", FullName = "Simultaneous Authentication of Equals", IsSecure = true, Type = SecurityProtocolType.KeyExchange } },
+            { "PBKDF", new SecurityProtocol { Name = "PBKDF", FullName = "Password-Based Key Derivation Function", IsSecure = true, Type = SecurityProtocolType.KeyDerivation } },
+            { "RADIUS", new SecurityProtocol { Name = "RADIUS", FullName = "Remote Authentication Dial-In User Service", IsSecure = true, Type = SecurityProtocolType.Authentication } },
+            { "EAP", new SecurityProtocol { Name = "EAP", FullName = "Extensible Authentication Protocol", IsSecure = true, Type = SecurityProtocolType.Authentication } },
         };
     }
-
-    public class SecurityStandard
+    public class SecurityProtocol
     {
         public string FullName { get; init; }
         public string Name { get; init; }
         public bool IsSecure { get; init; }
-        public int Priority { get; init; }
+        public int Priority { get; init; } = 0;
+        public SecurityProtocolType Type { get; init; }
     }
 
-    public class SecurityProtocols
+    public enum SecurityProtocolType
     {
-        public string FullName { get; init; }
-        public string Name { get; init; }
-        public bool IsSecure { get; init; }
-        public string Usage { get; init; }
-    }
-
-    public class EncryptionType
-    {
-        public string Name { get; init; }
-        public bool IsSecure { get; init; }
-        public int Priority { get; init; }
+        Framework,
+        Integrity,
+        Encryption,
+        EncryptionMode,
+        KeyExchange,
+        KeyDerivation,
+        Authentication
     }
 }
