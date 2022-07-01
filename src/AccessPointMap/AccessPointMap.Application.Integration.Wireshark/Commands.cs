0b994d0078fe6551470c7dcf206a3593230a0828
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2022-07-01 14:47:17 +0200

    WiresharkIntegrationSerivce implementation

---
+++ src/AccessPointMap/AccessPointMap.Application.Integration.Wireshark/Commands.cs
@@ -0,0 +1,13 @@
+﻿using AccessPointMap.Application.Integration.Core;
+using Microsoft.AspNetCore.Http;
+
+namespace AccessPointMap.Application.Integration.Wireshark
+{
+    public static class Commands
+    {
+        public class CreateAccessPointPacketsFromPcapFile : IIntegrationCommand
+        {
+            public IFormFile ScanPcapFile { get; set; } 
+        }
+    }
+}
---
+++ src/AccessPointMap/AccessPointMap.Application.Integration.Wireshark/WiresharkIntegrationService.cs
@@ -0,0 +1,87 @@
+﻿using AccessPointMap.Application.Integration.Core;
+using AccessPointMap.Application.Integration.Core.Exceptions;
+using AccessPointMap.Application.Oui.Core;
+using AccessPointMap.Application.Pcap.Core;
+using AccessPointMap.Domain.AccessPoints;
+using AccessPointMap.Infrastructure.Core.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccessPointMap.Application.Integration.Wireshark
+{
+    public class WiresharkIntegrationService : AccessPointIntegrationBase<WiresharkIntegrationService>, IWiresharkIntegrationService
+    {
+        private readonly string _adnnotationName = "Wireshark integration provided data";
+
+        private const string _integrationName = "Wireshark";
+        private const string _integrationDescription = "Integration for the world’s foremost and widely-used network protocol analyzer";
+        private const string _integrationVersion = "0.0.0-alpha";
+
+        protected override string IntegrationName => _integrationName;
+        protected override string IntegrationDescription => _integrationDescription;
+        protected override string IntegrationVersion => _integrationVersion;
+
+        public WiresharkIntegrationService(
+            IUnitOfWork unitOfWork,
+            IScopeWrapperService scopeWrapperService,
+            IPcapParsingService pcapParsingService,
+            IOuiLookupService ouiLookupService) : base(unitOfWork, scopeWrapperService, pcapParsingService, ouiLookupService) { }
+
+        public async Task Handle(IIntegrationCommand command)
+        {
+            switch (command)
+            {
+                case Commands.CreateAccessPointPacketsFromPcapFile cmd: await HandleCommand(cmd); break;
+
+                default: throw new IntegrationException($"This command is not supported by the {IntegrationName} integration.");
+            }
+        }
+
+        private async Task HandleCommand(Commands.CreateAccessPointPacketsFromPcapFile cmd)
+        {
+            if (cmd.ScanPcapFile is null)
+                throw new ArgumentNullException(nameof(cmd));
+
+            if (Path.GetExtension(cmd.ScanPcapFile.FileName).ToLower() != ".cap")
+                throw new ArgumentNullException(nameof(cmd));
+
+            var packetMap = await PcapParsingService.MapPacketsToMacAddresses(cmd.ScanPcapFile);
+
+            foreach (var map in packetMap)
+            {
+                await CreateAccessPointPackets(map.Key, map.Value);
+            }
+
+            await UnitOfWork.Commit();
+        }
+
+        private async Task CreateAccessPointPackets(string bssid, IEnumerable<Packet> packets)
+        {
+            if (!await UnitOfWork.AccessPointRepository.Exists(bssid)) return;
+
+            var accessPoint = await UnitOfWork.AccessPointRepository.Get(bssid);
+
+            foreach (var packet in packets)
+            {
+                accessPoint.Apply(new Events.V1.AccessPointPacketCreated
+                {
+                    Id = accessPoint.Id,
+                    SourceAddress = packet.SourceAddress,
+                    DestinationAddress = packet.DestinationAddress,
+                    FrameType = packet.FrameType,
+                    Data = packet.Data
+                });
+            }
+
+            accessPoint.Apply(new Events.V1.AccessPointAdnnotationCreated
+            {
+                Id = accessPoint.Id,
+                Title = _adnnotationName,
+                Content = $"Inserted {packets.Count()} IEEE 802.11 frames."
+            });
+        }
+    }
+}
