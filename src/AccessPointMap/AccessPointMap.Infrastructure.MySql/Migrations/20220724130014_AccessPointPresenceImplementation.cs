e4a25fd2a3ac18e9d687076b036319b29fee0fc9
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-07-24 21:22:58 +0200

    MySQL infrastructure presence feature migration

---
+++ src/AccessPointMap/AccessPointMap.Infrastructure.MySql/Migrations/20220724130014_AccessPointPresenceImplementation.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccessPointMap.Infrastructure.MySql.Migrations
+{
+    public partial class AccessPointPresenceImplementation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Presence_Value",
+                schema: "apm",
+                table: "AccessPoints",
+                type: "tinyint(1)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Presence_Value",
+                schema: "apm",
+                table: "AccessPoints");
+        }
+    }
+}
