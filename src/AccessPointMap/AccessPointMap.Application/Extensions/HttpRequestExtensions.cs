d6b0fb8b49ec921be6811dafe1e6421d9d757dd2
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-08-24 19:15:57 +0200

    Enh: Shared ip resolving extension method

---
+++ src/AccessPointMap/AccessPointMap.Application/Extensions/HttpRequestExtensions.cs
@@ -0,0 +1,27 @@
+﻿using AccessPointMap.Domain.Core.Extensions;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Linq;
+
+namespace AccessPointMap.Application.Extensions
+{
+    public static class HttpRequestExtensions
+    {
+        public static string GetIpAddressString(this HttpRequest httpRequest)
+        {
+            string ip = httpRequest.Headers["X-Forwared-For"].FirstOrDefault();
+
+            if (!ip.IsEmpty()) ip = ip.Split('.').First().Trim();
+
+            if (ip.IsEmpty()) ip = Convert.ToString(httpRequest.HttpContext.Connection.RemoteIpAddress);
+
+            if (ip.IsEmpty()) ip = Convert.ToString(httpRequest.HttpContext.Connection.LocalIpAddress);
+
+            if (ip.IsEmpty()) ip = httpRequest.Headers["REMOTE_ADDR"].FirstOrDefault();
+
+            if (ip.IsEmpty()) ip = string.Empty;
+
+            return ip;
+        }
+    }
+}
