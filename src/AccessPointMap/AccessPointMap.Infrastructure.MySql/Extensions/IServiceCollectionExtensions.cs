0b8d3f84e55996c7b019b792b3ddd141a2242826
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-10-12 00:07:45 +0200

    Enh: Referece-less persistance configuration

---
+++ src/AccessPointMap/AccessPointMap.Infrastructure.MySql/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using AccessPointMap.Infrastructure.Core.Abstraction;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+
+namespace AccessPointMap.Infrastructure.MySql.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMySqlInfrastructure(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<AccessPointMapDbContext>(options =>
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), options =>
+                {
+                    options.SchemaBehavior(MySqlSchemaBehavior.Translate, (schema, table) => $"{schema}_{table}");
+
+                    options.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
+                }));
+
+            services.AddScoped<IAuthenticationDataAccessService, AuthenticationDataAccessService>();
+
+            services.AddScoped<IDataAccess, DataAccess>();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
