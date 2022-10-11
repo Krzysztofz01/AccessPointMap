2651b4cbd984fabbad74bc9dab57f62db00af7ce
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-10-11 23:01:57 +0200

    Enh: Generic repository implementation

---
+++ src/AccessPointMap/AccessPointMap.Domain.Core/Contracts/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using AccessPointMap.Domain.Core.Models;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AccessPointMap.Domain.Core.Contracts
+{
+    public interface IRepository<TAggregateRoot> where TAggregateRoot : AggregateRoot
+    {
+        Task AddAsync(TAggregateRoot entity, CancellationToken cancellationToken = default);
+        Task<TAggregateRoot> GetAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<IQueryable<TAggregateRoot>> QueryAsync(Expression<Func<TAggregateRoot, bool>> predicate, CancellationToken cancellationToken = default);
+        Task<bool> ExistsAsync(Guid id, CancellationToken cancellationToken = default);
+    }
+}
