e97eec3c02183d9322f2092b817df39aa0aea9a7
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-10-13 15:36:23 +0200

    Enh: Contract updated

--- src/AccessPointMap/AccessPointMap.Application.Oui.Core/IOuiLookupService.cs
+++ src/AccessPointMap/AccessPointMap.Application.Oui.Core/IOuiLookupService.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AccessPointMap.Application.Oui.Core
 {
     public interface IOuiLookupService
     {
-        Task<string> GetManufacturerName(string macAddress);
-        Task<IDictionary<string, string>> GetManufacturerLookupDictionary(IEnumerable<string> macAddresses);
+        Task<string> GetManufacturerNameAsync(string macAddress, CancellationToken cancellationToken = default);
+        Task<IDictionary<string, string>> GetManufacturerLookupDictionaryAsync(IEnumerable<string> macAddresses, CancellationToken cancellationToken = default);
     }
 }
