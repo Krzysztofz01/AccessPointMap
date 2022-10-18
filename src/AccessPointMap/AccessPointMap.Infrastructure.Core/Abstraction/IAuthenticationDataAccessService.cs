5aa12e23865e40b8eae56b419547ea0cfaa7884b
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-10-18 19:44:47 +0200

    Enh: AuthDataAccess contract updated

--- src/AccessPointMap/AccessPointMap.Infrastructure.Core/Abstraction/IAuthenticationDataAccessService.cs
+++ src/AccessPointMap/AccessPointMap.Infrastructure.Core/Abstraction/IAuthenticationDataAccessService.cs
@@ -1,13 +1,14 @@
 ﻿using AccessPointMap.Domain.Identities;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AccessPointMap.Infrastructure.Core.Abstraction
 {
     public interface IAuthenticationDataAccessService
     {
-        Task<Identity> GetUserByEmail(string email);
-        Task<Identity> GetUserByRefreshToken(string refreshTokenHash);
-        Task<bool> UserWithEmailExsits(string email);
-        Task<bool> AnyUsersExists();
+        Task<Identity> GetUserOrDefaultByEmailAsync(string email, CancellationToken cancellationToken = default);
+        Task<Identity> GetUserOrDefaultByActiveRefreshTokenAsync(string refreshTokenHash, CancellationToken cancellationToken = default);
+        Task<bool> AnyUserWithEmailExsitsAsync(string email, CancellationToken cancellationToken = default);
+        Task<bool> AnyUserExistsAsync(CancellationToken cancellationToken = default);
     }
 }
