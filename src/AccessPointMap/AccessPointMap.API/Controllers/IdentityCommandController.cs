a2214bd54d48b83b315ea151f0c23a5a71310d90
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-10-25 22:42:44 +0200

    Enh: Identity controllers refactored to use ct

--- src/AccessPointMap/AccessPointMap.API/Controllers/IdentityCommandController.cs
+++ src/AccessPointMap/AccessPointMap.API/Controllers/IdentityCommandController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 using static AccessPointMap.Application.Identities.Commands;
 
@@ -23,15 +24,15 @@ namespace AccessPointMap.API.Controllers
         }
 
         [HttpDelete]
-        public async Task<IActionResult> Delete(V1.Delete command) =>
-            await Command(command, _identityService.Handle);
+        public async Task<IActionResult> Delete(V1.Delete command, CancellationToken cancellationToken) =>
+            await ExecuteCommandAsync(command, _identityService, cancellationToken);
 
         [HttpPut("activation")]
-        public async Task<IActionResult> Activation(V1.Activation command) =>
-            await Command(command, _identityService.Handle);
+        public async Task<IActionResult> Activation(V1.Activation command, CancellationToken cancellationToken) =>
+            await ExecuteCommandAsync(command, _identityService, cancellationToken);
 
         [HttpPut("role")]
-        public async Task<IActionResult> RoleChange(V1.RoleChange command) =>
-            await Command(command, _identityService.Handle);
+        public async Task<IActionResult> RoleChange(V1.RoleChange command, CancellationToken cancellationToken) =>
+            await ExecuteCommandAsync(command, _identityService, cancellationToken);
     }
 }
