2cf9d6f94e1f6af48e705a5e29d80eb0cc8c6492
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-10-25 20:43:45 +0200

    Enh: Queries related result extensions

---
+++ src/AccessPointMap/AccessPointMap.Application/Extensions/ResultExtensions.cs
@@ -0,0 +1,49 @@
+﻿using AccessPointMap.Application.Abstraction;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccessPointMap.Application.Extensions
+{
+    internal static class ResultExtensions
+    {
+        public static Result<T> ToResultObject<T>(this T obj) where T : class
+        {
+            if (obj is null) return Result.Failure<T>(NotFoundError.Default);
+
+            return Result.Success<T>(obj);
+        }
+
+        public static async Task<Result<T>> ToResultObjectAsync<T>(this Task<T> obj) where T : class
+        {
+            var objResult = await obj;
+            return objResult.ToResultObject();
+        }
+
+        public static Result<IEnumerable<TEntity>> ToResultObject<TEntity>(this IEnumerable<TEntity> entities) where TEntity : class
+        {
+            if (!entities.Any()) Result.Failure<IEnumerable<TEntity>>(NotFoundError.Default);
+
+            return Result.Success<IEnumerable<TEntity>>(entities);
+        }
+
+        public static Result<IEnumerable<TEntity>> ToResultObject<TEntity>(this List<TEntity> entities) where TEntity : class
+        {
+            if (entities.Count == 0) Result.Failure<IEnumerable<TEntity>>(NotFoundError.Default);
+
+            return Result.Success<IEnumerable<TEntity>>(entities);
+        }
+
+        public static async Task<Result<IEnumerable<TEntity>>> ToResultObjectAsync<TEntity>(this Task<IEnumerable<TEntity>> entities) where TEntity : class
+        {
+            var entitiesResult = await entities;
+            return entitiesResult.ToResultObject();
+        }
+
+        public static async Task<Result<IEnumerable<TEntity>>> ToResultObjectAsync<TEntity>(this Task<List<TEntity>> entities) where TEntity : class
+        {
+            var entitiesResult = await entities;
+            return entitiesResult.ToResultObject();
+        }
+    }
+}
