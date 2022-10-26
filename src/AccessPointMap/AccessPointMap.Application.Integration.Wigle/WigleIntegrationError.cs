9474eea586aece0ccdb6eb1491d4228482db4e09
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-10-26 19:11:26 +0200

    Enh: Wigle integration support for new contract

---
+++ src/AccessPointMap/AccessPointMap.Application.Integration.Wigle/WigleIntegrationError.cs
@@ -0,0 +1,13 @@
+﻿using AccessPointMap.Application.Integration.Core;
+
+namespace AccessPointMap.Application.Integration.Wigle
+{
+    internal class WigleIntegrationError : IntegrationError
+    {
+        protected WigleIntegrationError(string message) : base(message) { }
+
+        public static WigleIntegrationError UploadedCsvFileIsNull => new("The provided CSV file can not be accessed.");
+        public static WigleIntegrationError UploadedCsvGzFileIsNull => new("The provided CSV.GZ file can not be accessed.");
+        public static WigleIntegrationError UploadedFileHasInvalidFormat => new("The provided file format is not matching the requirements.");
+    }
+}
