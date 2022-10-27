2e08c6f92bc88c3c9c05a173cdf33c07b3512d2c
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-10-27 22:12:21 +0200

    Enh: Results implemented into the pcap parser

--- src/AccessPointMap/AccessPointMap.Application.Pcap.Core/IPcapParsingService.cs
+++ src/AccessPointMap/AccessPointMap.Application.Pcap.Core/IPcapParsingService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AccessPointMap.Application.Abstraction;
+using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,6 +8,6 @@ namespace AccessPointMap.Application.Pcap.Core
 {
     public interface IPcapParsingService
     {
-        public Task<IDictionary<string, List<Packet>>> MapPacketsToMacAddressesAsync(IFormFile pcapFile, CancellationToken cancellationToken = default);
+        public Task<Result<IDictionary<string, List<Packet>>>> MapPacketsToMacAddressesAsync(IFormFile pcapFile, CancellationToken cancellationToken = default);
     }
 }
---
+++ src/AccessPointMap/AccessPointMap.Application.Pcap.Core/PcapParserError.cs
@@ -0,0 +1,14 @@
+﻿using AccessPointMap.Application.Abstraction;
+
+namespace AccessPointMap.Application.Pcap.Core
+{
+    public class PcapParserError : Error
+    {
+        protected PcapParserError() { }
+        protected PcapParserError(string message) : base(message) { }
+
+        public static PcapParserError UploadedPcapFileIsNull => new("The provided PCAP/CAP file can not be accessed.");
+        public static PcapParserError UploadedFileHasInvalidFormat => new("The provided file format is not matching the requirements.");
+        public static PcapParserError FatalError => new("The CAP/PCAP format parser encountered an unexpected error.");
+    }
+}
