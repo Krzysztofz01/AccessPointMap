794ae955772dd943f43972b2a06df5af9c4d8a63
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-10-29 12:28:28 +0200

    Enh: Result support implemented into auth service

--- src/AccessPointMap/AccessPointMap.Application/Authentication/AuthenticationService.cs
+++ src/AccessPointMap/AccessPointMap.Application/Authentication/AuthenticationService.cs
@@ -1,10 +1,13 @@
-﻿using AccessPointMap.Application.Settings;
+﻿using AccessPointMap.Application.Core;
+using AccessPointMap.Application.Settings;
 using AccessPointMap.Domain.Core.Extensions;
 using AccessPointMap.Domain.Identities;
 using AccessPointMap.Infrastructure.Core.Abstraction;
 using Microsoft.Extensions.Options;
 using System;
+using System.Threading;
 using System.Threading.Tasks;
+using static AccessPointMap.Application.Authentication.AuthenticationErrors;
 
 namespace AccessPointMap.Application.Authentication
 {
@@ -44,14 +47,14 @@ namespace AccessPointMap.Application.Authentication
                 throw new ArgumentNullException(nameof(authorizationSettings));
         }
 
-        public async Task<Responses.V1.Login> Login(Requests.V1.Login request)
+        public async Task<Result<Responses.V1.Login>> Login(Requests.V1.Login request, CancellationToken cancellationToken = default)
         {
-            // TODO: Pass CancellationToken to the method
-            var identity = await _authenticationDataAccessService.GetUserOrDefaultByEmailAsync(request.Email) ??
-                throw new InvalidOperationException("Invalid authentication credentials.");
+            var identity = await _authenticationDataAccessService.GetUserOrDefaultByEmailAsync(request.Email, cancellationToken);
+            
+            if (identity is null) return Result.Failure<Responses.V1.Login>(AuthenticationCredentialsError.IdentityWithEmailNotFound);
 
             if (!_authenticationWrapperService.VerifyPasswordHashes(request.Password, identity.PasswordHash))
-                throw new InvalidOperationException("Invalid authentication credentials.");
+                return Result.Failure<Responses.V1.Login>(AuthenticationCredentialsError.IdentityAccessWithWrongPassword);
 
             string bearerToken = _authenticationWrapperService.GenerateJsonWebToken(identity);
 
@@ -67,19 +70,19 @@ namespace AccessPointMap.Application.Authentication
                 TokenHash = refreshTokenHash
             });
 
-            await _unitOfWork.Commit();
+            await _unitOfWork.Commit(cancellationToken);
 
-            return new Responses.V1.Login
+            return Result.Success(new Responses.V1.Login
             {
                 JsonWebToken = bearerToken,
                 RefreshToken = refreshToken
-            };
+            });
         }
 
-        public async Task Logout(Requests.V1.Logout request)
+        public async Task<Result> Logout(Requests.V1.Logout request, CancellationToken cancellationToken = default)
         {
-            // TODO: Pass the CancellationToken to the repository method
-            var identity = await _unitOfWork.IdentityRepository.GetAsync(_scopeWrapperService.GetUserId());
+            var identity = await _unitOfWork.IdentityRepository.GetAsync(_scopeWrapperService.GetUserId(), cancellationToken);
+            if (identity is null) return Result.Failure(AuthenticationIdentityError.UserNotFound);
 
             string refreshTokenHash = _authenticationWrapperService.HashString(request.RefreshToken);
 
@@ -90,16 +93,18 @@ namespace AccessPointMap.Application.Authentication
                 TokenHash = refreshTokenHash
             });
 
-            await _unitOfWork.Commit();
+            await _unitOfWork.Commit(cancellationToken);
+
+            return Result.Success();
         }
 
-        public async Task PasswordReset(Requests.V1.PasswordReset request)
+        public async Task<Result> PasswordReset(Requests.V1.PasswordReset request, CancellationToken cancellationToken = default)
         {
-            // TODO: Pass the CancellationToken to the repository method
-            var identity = await _unitOfWork.IdentityRepository.GetAsync(_scopeWrapperService.GetUserId());
+            var identity = await _unitOfWork.IdentityRepository.GetAsync(_scopeWrapperService.GetUserId(), cancellationToken);
+            if (identity is null) return Result.Failure(AuthenticationIdentityError.UserNotFound);
 
             if (!_authenticationWrapperService.ValidatePasswords(request.Password, request.PasswordRepeat))
-                throw new InvalidOperationException("Invalid authentication credentials");
+                return Result.Failure(AuthenticationCredentialsError.CredentialsNotMatching);
 
             var passwordHash = _authenticationWrapperService.HashPassword(request.Password);
 
@@ -109,21 +114,20 @@ namespace AccessPointMap.Application.Authentication
                 PasswordHash = passwordHash
             });
 
-            await _unitOfWork.Commit();
+            await _unitOfWork.Commit(cancellationToken);
+
+            return Result.Success();
         }
 
-        public async Task<Responses.V1.Refresh> Refresh(Requests.V1.Refresh request)
+        public async Task<Result<Responses.V1.Refresh>> Refresh(Requests.V1.Refresh request, CancellationToken cancellationToken = default)
         {
-            if (request.RefreshToken.IsEmpty())
-                throw new InvalidOperationException("Invalid authentication credentials.");
+            if (request.RefreshToken.IsEmpty()) return Result.Failure<Responses.V1.Refresh>(AuthenticationCredentialsError.InvalidRefreshToken);
 
             var refreshTokenHash = _authenticationWrapperService.HashString(request.RefreshToken);
 
-            // TODO: Pass CancellationToken to the method
-            var identity = await _authenticationDataAccessService.GetUserOrDefaultByActiveRefreshTokenAsync(refreshTokenHash);
-            // TODO: This is a dirty work-around. Use the result object later.
-            if (identity is null) return null;
-        
+            var identity = await _authenticationDataAccessService.GetUserOrDefaultByActiveRefreshTokenAsync(refreshTokenHash, cancellationToken);
+            if (identity is null) return Result.Failure<Responses.V1.Refresh>(AuthenticationCredentialsError.InvalidRefreshToken);
+
             var bearerToken = _authenticationWrapperService.GenerateJsonWebToken(identity);
 
             var replacementToken = _authenticationWrapperService.GenerateRefreshToken();
@@ -139,23 +143,22 @@ namespace AccessPointMap.Application.Authentication
                 ReplacementTokenHash = replacementTokenHash
             });
 
-            await _unitOfWork.Commit();
+            await _unitOfWork.Commit(cancellationToken);
 
-            return new Responses.V1.Refresh
+            return Result.Success(new Responses.V1.Refresh
             {
                 JsonWebToken = bearerToken,
                 RefreshToken = replacementToken
-            };
+            });
         }
 
-        public async Task Register(Requests.V1.Register request)
+        public async Task<Result> Register(Requests.V1.Register request, CancellationToken cancellationToken = default)
         {
-            // TODO: Pass CancellationToken to the method
-            if (await _authenticationDataAccessService.AnyUserWithEmailExsitsAsync(request.Email))
-                throw new InvalidOperationException("Invalid authentication credentials");
+            if (await _authenticationDataAccessService.AnyUserWithEmailExsitsAsync(request.Email, cancellationToken))
+                return Result.Failure(AuthenticationCredentialsError.IdentityWithEmailAlreadyExist);
 
             if (!_authenticationWrapperService.ValidatePasswords(request.Password, request.PasswordRepeat))
-                throw new InvalidOperationException("Invalid authentication credentials");
+                return Result.Failure(AuthenticationCredentialsError.CredentialsNotMatching);
 
             string passwordHash = _authenticationWrapperService.HashPassword(request.Password);
 
@@ -167,8 +170,7 @@ namespace AccessPointMap.Application.Authentication
                 PasswordHash = passwordHash
             });
 
-            // TODO: Pass CancellationToken to the method
-            if (_authorizationSettings.PromoteFirstAccount && !await _authenticationDataAccessService.AnyUserExistsAsync())
+            if (_authorizationSettings.PromoteFirstAccount && !await _authenticationDataAccessService.AnyUserExistsAsync(cancellationToken))
             {
                 identity.Apply(new Events.V1.IdentityActivationChanged
                 {
@@ -183,10 +185,11 @@ namespace AccessPointMap.Application.Authentication
                 });
             }
 
-            // TODO: Pass the CancellationToken to the repository method
-            await _unitOfWork.IdentityRepository.AddAsync(identity);
+            await _unitOfWork.IdentityRepository.AddAsync(identity, cancellationToken);
 
-            await _unitOfWork.Commit();
+            await _unitOfWork.Commit(cancellationToken);
+
+            return Result.Success();
         }
     }
 }
