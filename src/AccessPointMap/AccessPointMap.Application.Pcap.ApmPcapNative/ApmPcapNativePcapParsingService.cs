64e3ccf8d40d6e25da1062d204866adaef92d1a2
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-11-04 16:07:41 +0100

    Enh: Error loggign added to PCAP parsing service

--- src/AccessPointMap/AccessPointMap.Application.Pcap.ApmPcapNative/ApmPcapNativePcapParsingService.cs
+++ src/AccessPointMap/AccessPointMap.Application.Pcap.ApmPcapNative/ApmPcapNativePcapParsingService.cs
@@ -1,6 +1,7 @@
 ﻿using AccessPointMap.Application.Core;
 using AccessPointMap.Application.Pcap.Core;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -12,7 +13,15 @@ namespace AccessPointMap.Application.Pcap.ApmPcapNative
 {
     internal sealed class ApmPcapNativePcapParsingService : IPcapParsingService
     {
+        private readonly ILogger<ApmPcapNativePcapParsingService> _logger;
+        
         private readonly string[] _allowedExtensions = new string[] { ".cap", ".pcap", "pcapng" };
+
+        public ApmPcapNativePcapParsingService(ILogger<ApmPcapNativePcapParsingService> logger)
+        {
+            _logger = logger ??
+                throw new ArgumentNullException(nameof(logger));
+        }
 
         public async Task<Result<IDictionary<string, List<Packet>>>> MapPacketsToMacAddressesAsync(IFormFile pcapFile, CancellationToken cancellationToken = default)
         {
@@ -46,9 +55,12 @@ namespace AccessPointMap.Application.Pcap.ApmPcapNative
             {
                 throw;
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                throw;
+                _logger.LogWarning("Application service: {ServiceName} failure.\n   {Exception}",
+                    typeof(ApmPcapNativePcapParsingService).Name, ex);
+
+                return Result.Failure<IDictionary<string, List<Packet>>>(PcapParserError.FatalError);
             }
         }
     }
