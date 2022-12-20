b741cd46b072ef99586c080ce62a3fd64a2bf5bc
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-12-20 12:38:20 +0100

    Enh: Database settings updated

--- src/AccessPointMap/AccessPointMap.Application/Settings/DatabaseSettings.cs
+++ src/AccessPointMap/AccessPointMap.Application/Settings/DatabaseSettings.cs
@@ -2,6 +2,9 @@
 {
     public class DatabaseSettings
     {
+        public string Driver { get; set; }
         public string ConnectionString { get; set; }
+        public bool ApplyMigrations { get; set; }
+
     }
 }
